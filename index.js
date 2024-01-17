2bc029b8ff1e7e727e26542ab6a801480ed3c5ff
Engdawork Kassaye <engdaworkkassaye6@gmail.com>
2024-01-17 07:59:29 -0600

    user input works

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const readline = require('readline-sync');
+function getUserInput(prompt) {
+    return readline.question(prompt);
+  }
+  function main() {
+    console.log('=== Logo Generator ===');
+    
+ 
+    const text = getUserInput('Enter up to three characters for the text: ');
+  
+    
+  }
+  main();
+
+  
