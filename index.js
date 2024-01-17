26a4704d8d765a98bb28b59859f005cc89d79014
Engdawork Kassaye <engdaworkkassaye6@gmail.com>
2024-01-17 08:33:46 -0600

    shape and shape colour works

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const fs = require('fs');
 const readline = require('readline-sync');
 const { createLogoSVG } = require('./logoGenerator');
+
 function getUserInput(prompt) {
-    return readline.question(prompt);
+  return readline.question(prompt);
+}
+
+function main() {
+  
+  
+  
+  const text = getUserInput('Enter up to three characters for the text: ');
+
+  const textColor = getUserInput('Enter text color (keyword or hexadecimal): ');
+  const shapes = ['circle', 'triangle', 'square'];
+  const selectedShape = readline.keyInSelect(shapes, 'Select a shape: ');
+
+  if (selectedShape === -1) {
+    console.log('Exiting...');
+    return;
   }
-  function main() {
-    console.log('=== Logo Generator ===');
-    
+
+  const shapeColor = getUserInput('Enter shape color (keyword or hexadecimal): ');
+
  
-    const text = getUserInput('Enter up to three characters for the text: ');
-    const textColor = getUserInput('Enter text color (keyword or hexadecimal): ');
-    const svgContent = createLogoSVG(text,textColor);
-    fs.writeFileSync('logo.svg', svgContent);
-    
-  }
-  main();
+  const svgContent = createLogoSVG(text, textColor, shapes[selectedShape], shapeColor);
 
   
+  fs.writeFileSync('logo.svg', svgContent);
+
+  
+}
+
+
+main();
